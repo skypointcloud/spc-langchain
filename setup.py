cc27fea9578b516cbb1aa06f7598f80a4f889a28
Arun raja <arun.raja@skypointcloud.com>
2023-08-25 13:47:33 +0530

    Fixing existing issues with langchain

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="spc-langchain",
-    version="0.0.14",
+    version="0.0.16",
     author="Arun raja",
     author_email="arun.raja@skypointcloud.com",
     description="A package that contains langchain python code and modules for all SkyPoint Cloud projects",
