8abc0ffa3b71ac18b978ea3990e8805f78a3ddaa
Arun raja <arun.raja@skypointcloud.com>
2023-08-25 10:03:28 +0530

    Changes to convert langchain to module

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spc-langchain",
+    version="0.0.9",
+    author="Arun raja",
+    author_email="arun.raja@skypointcloud.com",
+    description="A package that contains langchain python code and modules for all SkyPoint Cloud projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/skypointcloud/spc-langchain.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "libs/langchain/src"},
+    packages=setuptools.find_packages(where="libs/langchain/src"),
+    python_requires=">=3.8",
+    install_requires=[
+        
+    ],
+)
