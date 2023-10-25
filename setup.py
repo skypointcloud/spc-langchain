0041afe507b9445d452ee8421c56827c22caa4b9
Arun raja <arun.raja@skypointcloud.com>
2023-10-25 12:12:53 +0530

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="spc-langchain",
-    version="0.0.40",
+    version="0.0.41",
     author="Arun raja",
     author_email="arun.raja@skypointcloud.com",
     description="SkyPoint fork of the LangChain library.",
